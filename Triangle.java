11280a6ae644626d4420f2c5227f127c5c10c3fe
yjqian02 <yjqian02@gmail.com>
2021-02-09 19:13:46 -0600

    Add files via upload
    Adding project1 files - Alice

---
+++ Triangle.java
@@ -0,0 +1,52 @@
+public class Triange(){
+    public Triangle(double x, double y, double w, double h){
+      x_pos = x;
+      y_pos = y;
+      width = w;
+      height = h;
+
+    public double calculatePerimeter(){
+        return x_pos + y_pos + width
+    }
+
+    public double calculateArea(){
+        return width * height * 0.5
+    }
+
+    public void setColor(Color c){
+        color = c;
+    }
+
+    public void setPos(double x, double y){
+        position = [x, y];
+    }
+
+    public double setHeight(double h){
+        height = h;
+    }
+
+    public double setWidth(){
+        width = w;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public double getXPos(){
+        return x_pos;
+    }
+
+    public double getYPos(){
+        return y_pos;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getWidth(){
+        reutrn width;
+    }
+}
+
